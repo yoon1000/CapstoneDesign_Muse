db9c0d1ed24fe7f15d9d2e19fa69390d5b8f93de
yoon1000 <lucayang@naver.com>
2019-11-18 21:26:42 +0900

    UI업데이트(11/18)

---
+++ android_11_10/application(muse)/app/src/main/java/com/ajou/capstonedesign/museapplication/FragmentHome.java
@@ -0,0 +1,32 @@
+package com.ajou.capstonedesign.museapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class FragmentHome extends Fragment {
+
+    private RecyclerView recyclerView;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+
+
+
+
+
+    }
+
+}
+
+
---
+++ android_11_10/application(muse)/app/src/main/java/com/ajou/capstonedesign/museapplication/RetrofitCommunication.java
@@ -0,0 +1,15 @@
+package com.ajou.capstonedesign.museapplication;
+
+import com.google.gson.JsonObject;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface RetrofitCommunication {
+    @POST("/users/register")
+    Call<JsonObject> userRegister(@Body JsonObject userData);
+
+    @POST("/users/login")
+    Call<JsonObject> userLogin(@Body JsonObject userData);
+}
